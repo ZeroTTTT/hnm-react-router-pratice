db063de2e2812b41babace657266e7172a9318a1
Youngho Park <dudgh4651@gmail.com>
2024-03-18 21:22:42 +0900

    navbar

---
+++ src/component/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser} from '@fortawesome/free-regular-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+
+const Navbar = () => {
+
+    const menulist = ['여성', 'Divided', '남성', '신생아/유아', '아동', 'H&M Home', 'Sale', '지속가능성']
+
+  return (
+    <div>    
+        <div className='login-button'>
+            <FontAwesomeIcon icon={faUser} />
+            <div>로그인</div>
+        </div>
+        <div className='nav-section'>
+            <img width={100} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/2560px-H%26M-Logo.svg.png" />
+        </div>
+        <div className='menu-area'>
+            <ul className='menu-list'>
+                {menulist.map(menu => <li>{menu}</li>) }
+            </ul>
+            <div>
+                <FontAwesomeIcon icon={faSearch} />
+                <input className='input-search' type='text' placeholder="제품검색"/>                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
